7f79a995171623fcb43bb2535597ba69ee3e89a9
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-02-27 12:52:45 -0300

    feat: app routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import AppRoutes from './routes'
+import GlobalStyles from './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <GlobalStyles />
+    <AppRoutes />
+  </React.StrictMode>
 )
