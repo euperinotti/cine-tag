89879c5a205a959f0d1dff32f0dc8520d26b8a51
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-02-28 12:49:41 -0300

    feat: add footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+export const Footer = () => {
+  return (
+    <S.Container>
+      <S.Heading>Desenvolvido por Alura</S.Heading>
+    </S.Container>
+  )
+}
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,13 @@
+import theme from '@/styles/theme'
+import styled from 'styled-components'
+
+export const Container = styled.footer`
+  display: flex;
+  justify-content: center;
+  background-color: ${theme.colors.black};
+`
+
+export const Heading = styled.h2`
+  font-weight: 400;
+  color: ${theme.colors.white};
+`
