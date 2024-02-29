ccb60c9120b1096355c4f2cd7e776fa6623ebcda
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-02-29 00:06:36 -0300

    refactor: reoutes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,23 +1,21 @@
 import Home from '@/pages/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Footer } from './components/Footer'
-import { Header } from './components/Header'
-import { FavoritesProvider } from './context/FavoritesContext'
+import PageNotFound from './pages/404'
+import BasePage from './pages/Base'
 import Favorites from './pages/Favorites'
 import Player from './pages/Player'
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
-      <Header />
-      <FavoritesProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/:id" element={<Player />} />
-        </Routes>
-      </FavoritesProvider>
-      <Footer />
+      <Routes>
+        <Route path="/" element={<BasePage />}>
+          <Route index element={<Home />} />
+          <Route path="favorites" element={<Favorites />} />
+          <Route path=":id" element={<Player />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   )
 }
