4bd3312de2334d70a6766b2ea8850876dc9f5d15
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-02-28 23:50:51 -0300

    feat: add dynamic route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,23 +1,22 @@
 import Home from '@/pages/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { BaseContainer } from './components/BaseContainer'
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
-import Favorites from './pages/Favorites'
 import { FavoritesProvider } from './context/FavoritesContext'
+import Favorites from './pages/Favorites'
+import Player from './pages/Player'
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Header />
-      <BaseContainer>
-        <FavoritesProvider>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/favorites" element={<Favorites />} />
-          </Routes>
-        </FavoritesProvider>
-      </BaseContainer>
+      <FavoritesProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/:id" element={<Player />} />
+        </Routes>
+      </FavoritesProvider>
       <Footer />
     </BrowserRouter>
   )
