9258746bf1ed6c2cf4f33ef14e165439577822e5
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-02-29 00:05:39 -0300

    feat: add 404 page

---
+++ src/pages/404/index.tsx
@@ -0,0 +1,10 @@
+import * as S from './styles'
+
+export default function PageNotFound() {
+  return (
+    <S.Container>
+      <h2>Ops!</h2>
+      <p>O conteúdo que você procura não foi encontrado</p>
+    </S.Container>
+  )
+}
